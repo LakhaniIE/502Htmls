27ecd6620aa06eb57e484bbb0269f760cf124a25
Adnan Lakhani <adnan@lakhaniproductions.com>
2022-04-14 14:38:27 -0400

    updated js file

--- SiteAssets/RhyBus/Js/script.js
+++ SiteAssets/RhyBus/Js/script.js
@@ -1,22 +1,6 @@
-// $(document).ready(function(){
-//     activeLinkControl()
-
-// });
-
-
-
-// function activeLinkControl(){
-//     $('.menu li a').click(function(){
-//         $('li').removeClass('anchoractive')
-//         $(this).closest('.nav-tem').addClass('anchoractive')
-//     })
-// }
-
 const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll('.menu li a');
 navLinks.forEach(link=>{
-
-    console.log(link.href);
 
     if(link.href.includes(`${activePage}`) ){
         link.classList.add('anchoractive')
